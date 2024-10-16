eb9c576b8058d77d5f7ec70e58fcff0052d489b6
Mohammed Saeed <105430093+SA33ED@users.noreply.github.com>
2024-10-16 11:18:13 +0300

    Add Token to the headers

--- lib/core/databases/api/api_interceptors.dart
+++ lib/core/databases/api/api_interceptors.dart
@@ -1,4 +1,7 @@
 import 'package:dio/dio.dart';
+import 'package:eventown/core/constants/app_constants.dart';
+import 'package:eventown/core/databases/cache/cache_helper.dart';
+import 'package:eventown/core/services/service_locator.dart';
 
 class ApiInterceptors extends Interceptor {
   @override
@@ -7,6 +10,10 @@ class ApiInterceptors extends Interceptor {
     RequestInterceptorHandler handler,
   ) {
     // Token Will be added here to the header if it is not null.
+    options.headers["Authorization"] =
+        sl<CacheHelper>().getData(key: AppConstants.token) != null
+            ? '${sl<CacheHelper>().getData(key: AppConstants.token)}'
+            : null;
 
     super.onRequest(options, handler);
   }
